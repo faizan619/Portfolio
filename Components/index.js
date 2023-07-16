ebeb30366af0644d57010cbf2fa3d021c6e07be0
faizan alam <alamf6023@gmail.com>
2023-07-16 17:11:21 +0530

    feat: added animation and icons

---
+++ Components/index.js
@@ -0,0 +1,48 @@
+const urlCopy = ()=>{
+    var url = window.location.href;
+    navigator.clipboard.writeText(url);
+    
+    var text = "Copied"
+    const clip = document.querySelector(".sharingLink");
+    clip.innerHTML = text;
+    button.classList.add("copied");
+    setTimeout(() => {
+        button.classList.remove("copied");
+    },3000)
+}
+
+// gsap.from(".HeaderSection",{
+//     y:10,
+//     duration:1,
+// })
+
+const time = gsap.timeline();
+
+gsap.to(".HeaderSection",{
+    opacity:1,
+    scrollTrigger:{
+        scrub:true,
+        trigger:".HeaderSection",
+        start:'top -10%',
+        end:'bottom 2%',
+    }
+})
+    time.to(".HeaderSection",{
+        opacity:0,
+    })
+    .from(".userPhoto",{
+        scale:2,
+        y:-50,
+    })
+    .to(".userPhoto",{
+        scale:1.2,
+        y:-10,
+    })
+    .from(".UserName",{
+        opacity:0,
+    })
+    .from(".UserInfo",{
+        y:10,
+        repeat:-1,
+        yoyo:true,
+    })
